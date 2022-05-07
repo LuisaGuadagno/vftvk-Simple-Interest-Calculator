49ca7b35ff54f8587edc8da6f7936994f84a1aaf
LuisaGuadagno <63742478+LuisaGuadagno@users.noreply.github.com>
2022-05-07 16:05:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-function compute()
+  function updateRate() 
 {
-    p = document.getElementById("principal").value;
-    
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
+
+
+        function compute()
+        {
+            var principal = document.getElementById("principal").value;
+            var rate = document.getElementById("rate").value;
+            var years = document.getElementById("years").value;
+            var interest = principal * years * rate /100;
+            var year = new Date().getFullYear()+parseInt(years);
+            
+            
+           
+            document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+            
+            
+           
         
+        
+        }
+
+
+
+        function validation(){
+            //Create references to the input elements we wish to validate
+            var principal = document.getElementById("principal");
+            
+            if(principal.value <= 0 ){
+                  alert("Enter a positive number!");
+                  principal.focus();
+                  return true;
+            }
+          
+           //If all is well return true.
+            return true;
+      }
